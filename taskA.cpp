28c649155e783f033c206ead9b1138070357bac6
dm1trysh <dmitr20481024@gmail.com>
2022-12-09 02:25:08 +0300

    changed variables type to size_t

--- taskA.cpp
+++ taskA.cpp
@@ -1,45 +1,36 @@
 #include <iostream>
 
 bool BinarySearch(const int* begin, const int* end, int target) {
-    int left = 0;
-    int right = end - begin;
-    while (right > left + 1) {
-        int mid = (left + right) / 2;
-        if (*(begin + mid) <= target) {
-            left = mid;
-        }
-        else {
-            right = mid;
-        }
+  int left = 0;
+  int right = end - begin;
+  while (right > left + 1) {
+    int mid = (left + right) / 2;
+    if (*(begin + mid) <= target) {
+      left = mid;
     }
-    return (target == *(begin + left));
+    else {
+      right = mid;
+    }
+  }
+  return (target == *(begin + left));
 }
 
 int main() {
-    int number;
-    std::cin >> number;
-    int* array = new int[number];
-    for (int i = 0; i < number; i++) {
-        std::cin >> array[i];
-    }
-    int questions;
-    std::cin >> questions;
-    for (int i = 0; i < questions; i++) {
-        int left = 0;
-        int right = 0;
-        int asked = 0;
-        std::cin >> left;
-        std::cin >> right;
-        std::cin >> asked;
-        int* ptr = array;
-        bool result = BinarySearch(ptr + left, ptr + right, asked);
-        if (result) {
-            std::cout << "YES\n";
-        }
-        else {
-            std::cout << "NO\n";
-        }
-    }
-    delete[] array;
-    return 0;
+  size_t number;
+  std::cin >> number;
+  int* array = new int[number];
+  for (size_t i = 0; i < number; i++) {
+    std::cin >> array[i];
+  }
+  size_t questions;
+  std::cin >> questions;
+  for (size_t i = 0; i < questions; i++) {
+    int left = 0, right = 0, asked = 0;
+    std::cin >> left >> right >> asked;
+    int* ptr = array;
+    bool result = BinarySearch(ptr + left, ptr + right, asked);
+    std::cout << result ? "YES\n" : "NO\n";
+  }
+  delete[] array;
+  return 0;
 }
